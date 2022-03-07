cfa3f969f0e2c206a00c354f53d74ce5c9432f8f
unknown <qwhick2@gmail.com>
2022-03-07 17:17:23 -0500

    Completed Entry Chart Tracker Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { useState } from 'react';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries'
+import AddEntryForm from './Components/AddEntry/AddEntryForm';
+import EntriesChartTracker from './Components/EntriesChartTracker/EntriesChartTracker';
 
 function App() {
 
   const [entries, setEntries] = useState([{weight: 175, date: '11-23-2021'}, {weight: 198, date: '11-25-2021'}])
 
+  function addNewEntry(entry){
+
+    let tempEntries = [...entries, entry]
+
+    setEntries(tempEntries)
+  }
+
   return (
     <div>
       <DisplayEntries parentEntries={entries} />
+      <AddEntryForm addNewEntryProperty={addNewEntry} />
+      <EntriesChartTracker parentEntries={entries} />
 
     </div>
   );
